48e23101b577c6ee396671087da698849e63e984
Sparrow Paul <sparrowpaul2@gmail.com>
2018-07-22 20:49:57 +0000

    initial commit

--- app/src/main/java/com/sparrowpaul/splashrotation/Splash.java
+++ app/src/main/java/com/sparrowpaul/splashrotation/Splash.java
@@ -10,17 +10,16 @@ import android.widget.ImageView;
 public class Splash extends Activity {
     int splashTimer = 3000;
     private ImageView imageView;
-    boolean dirY = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        imageView = findViewById(R.id.imageViewID);
+        imageView = findViewById(R.id.imageViewID); //image to be rotated
 
         rotateImage();
 
-        new Handler().postDelayed(new Runnable() {
+        new Handler().postDelayed(new Runnable() { // to ensure that the splash screen loads for 3 seconds
             @Override
             public void run() {
                 Intent intent = new Intent(Splash.this, MainActivity.class);
@@ -31,7 +30,7 @@ public class Splash extends Activity {
 
     }
 
-    public void rotateImage(){
+    public void rotateImage(){ // code to rotate the image
             ObjectAnimator flip = ObjectAnimator.ofFloat(imageView, "rotationY", 0f, 180f);
             flip.setDuration(3000);
             flip.start();
